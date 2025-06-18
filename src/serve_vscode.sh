22c2e8631db7ac5ffffbbfbf400341d9958b40f0
Ryan Friedman <ryanfriedman22@gmail.com>
2025-06-17 22:36:31 -0700

    Enable specification of R and Python versions (#3)
    * Improved argument parsing, allow optional specification of R/Python versions
    
    * Update README
    
    * typo fix
    
    * R needs to be in caps
    
    * Check for errors loading modules before echoing the auth message
    
    * Improve messaging

--- src/serve_vscode.sh
+++ src/serve_vscode.sh
@@ -1,42 +1,60 @@
 #!/bin/bash
-USAGE="""
-Usage: serve_vscode [-m memory] [-c cores] [-t timelimit] [-n node]
-Options:
-  -m memory     Memory to allocate for the job (default: 8G)
-  -c cores      Number of cores to allocate for the job (default: 1)
-  -t timelimit  Time limit for the job, formatted as hours:minutes:seconds (default: 48:0:0)
-  -n node       What node to use as the server (default: first available)
-  -h            Display this help message
-"""
-while getopts "m:c:t:n:h" opt; do
-    case ${opt} in
-        m )
-            MEM=$OPTARG
+USAGE="Usage: serve_vscode [-m|--memory memory] [-c|--cores cores] [-t|--timelimit timelimit] [-r|--r_version r_version] [-p|--python_version python_version]\n\
+Options:\n\
+  -m, --memory     Memory to allocate for the job (default: 8G)\n\
+  -c, --cores      Number of cores to allocate for the job (default: 1)\n\
+  -t, --timelimit  Time limit for the job, formatted as hours:minutes:seconds (default: 48:0:0)\n\
+  -r, --r_version  R version to use, formatted as 'x.x.x'. Default is to use what is in '~/.bashrc' or equivalent\n\
+  -p, --python_version  Python version to use, formatted as 'x.x.x'. Default is to use what is in '~/.bashrc' or equivalent\n\
+  -h, --help       Show this help message and exit\n"
+
+# Defaults
+MEM="8G"
+CORES="1"
+TIMELIMIT="48:0:0"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -m|--memory)
+            MEM="$2"
+            shift
+            shift
             ;;
-        c )
-            CORES=$OPTARG
+        -c|--cores)
+            CORES="$2"
+            shift
+            shift
             ;;
-        t )
-            TIMELIMIT=$OPTARG
+        -t|--timelimit)
+            TIMELIMIT="$2"
+            shift
+            shift
             ;;
-		n )
-			NODE=$OPTARG
-			;;
-        h )
-            echo "$USAGE"
+        -r|--r_version)
+            R_VERSION="$2"
+            shift
+            shift
+            ;;
+        -p|--python_version)
+            PYTHON_VERSION="$2"
+            shift
+            shift
+            ;;
+        -h|--help)
+            echo -e "${USAGE}"
             exit 0
             ;;
-        \? )
-            echo "$USAGE"
+        -*|--*)
+            echo "Unknown option: $1"
+            echo -e "${USAGE}"
+            exit 1
+            ;;
+        *)
+            echo -e "${USAGE}"
             exit 1
             ;;
     esac
 done
-
-# Set default values if not provided
-MEM=${MEM:-8G}
-CORES=${CORES:-1}
-TIMELIMIT=${TIMELIMIT:-48:0:0}
 
 LOG_FILE=$HOME/nobackup/log/vscode.o
 ERR_FILE=$HOME/nobackup/log/vscode.e
@@ -74,11 +92,14 @@ if [ -f "${ERR_FILE}" ]; then
     rm "${ERR_FILE}"
 fi
 
-FLAGS="-o ${LOG_FILE} -e ${ERR_FILE} -l mfree=${MEM} -pe serial ${CORES} -l h_rt=${TIMELIMIT} -N vscode"
-if [ -n "${NODE}" ]; then
-    FLAGS="${FLAGS} -l hostname=${NODE}"
+cmd="qsub -o ${LOG_FILE} -e ${ERR_FILE} -l mfree=${MEM} -pe serial ${CORES} -l h_rt=${TIMELIMIT} -N vscode ${HOME}/sge/serve_vscode.sge"
+# Add R and Python versions to the command if specified
+if [ -n "${R_VERSION}" ]; then
+    cmd+=" -r ${R_VERSION}"
 fi
-cmd="qsub ${FLAGS} ${HOME}/sge/serve_vscode.sge"
+if [ -n "${PYTHON_VERSION}" ]; then
+    cmd+=" -p ${PYTHON_VERSION}"
+fi
 echo -e "Submitting a VSCode server with the command:\n\t${cmd}\n\n"
 eval "${cmd}"
 
@@ -93,40 +114,43 @@ if [ -f "${LOG_FILE}" ]; then
     max_attempts=30
     attempts=0
     
+    AUTH_MSG=""
     while [ $attempts -lt $max_attempts ]; do
         # Check if the GitHub login line exists in the log file
         if grep -q "Found token in keyring" "${LOG_FILE}"; then
-            echo ""
-            echo "Server is already authenticated. Open VS Code, select 'Connect to Tunnel...' and then 'GitHub' to connect to the server."
-            echo ""
+            AUTH_MSG="Server is already authenticated. Open VS Code, select 'Connect to Tunnel...' and then 'GitHub' to connect to the server."
             break
         elif grep -q "To grant access to the server, please log into" "${LOG_FILE}"; then
             # Extract and display the line containing the GitHub login URL and code
             github_line=$(grep "To grant access to the server, please log into" "${LOG_FILE}")
-            echo ""
-            echo "-------------------------------------------------------"
-            echo "$github_line"
-            echo "-------------------------------------------------------"
-            echo ""
-            echo "After authenticating, open VS Code, select 'Connect to Tunnel...' and then 'GitHub' to connect to the server."
-            echo ""
+            AUTH_MSG="\n-------------------------------------------------------\n$github_line\n-------------------------------------------------------\n\nAfter authenticating, open VS Code, select 'Connect to Tunnel...' and then 'GitHub' to connect to the server."
             break
         fi
-        
         # Increment attempts counter
         attempts=$((attempts + 1))
         # Wait for 1 second before checking again
         sleep 1
     done
-    
     # If we couldn't find the line after all attempts
     if [ $attempts -eq $max_attempts ]; then
-        echo "Could not find GitHub login information after ${max_attempts} seconds."
-        echo "Please check the log file manually at: ${LOG_FILE}"
-        echo "If you encounter issues, examine the error file at: ${ERR_FILE}"
+        AUTH_MSG="Could not find GitHub login information after ${max_attempts} seconds.\nPlease check the log file manually at: ${LOG_FILE}\nIf you encounter issues, examine the error file at: ${ERR_FILE}"
     fi
 else
     echo "Log file not found at ${LOG_FILE} after waiting ${WAIT_TIME} seconds for file creation."
     echo "The job may still be in the queue or there might be an issue."
     echo "You can check job status with 'qstat -u $USER' and examine error output at: ${ERR_FILE}"
+    exit 0
 fi
+
+sleep 1
+
+# Check for errors loading modules
+module_errors=$(grep "ERROR: Unable to locate a modulefile for" ~/nobackup/log/vscode.e 2>/dev/null)
+if [ -n "$module_errors" ]; then
+    echo "Error loading modules. Check your module versions are valid. Error message:"
+    echo "$module_errors"
+    exit 1
+fi
+
+# Display the authentication message
+echo -e "$AUTH_MSG"
